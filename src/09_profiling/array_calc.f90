42945866062fc2f757965e160bb63f6eb0d4b568
Jaomi Widmann <m300912@levante0.lvt.dkrz.de>
2023-08-23 16:04:47 +0200

    Simplified code in array_calc.f90

--- src/09_profiling/array_calc.f90
+++ src/09_profiling/array_calc.f90
@@ -17,7 +17,7 @@ program matrix_multi_test
 
     ! Time measurement:
     integer, parameter :: maxsize = 1000
-    integer, parameter :: step    = 100
+    integer, parameter :: step    = 200
     integer, parameter :: timesteps = maxsize/step 
     integer :: count, rate
     real(wp)    :: timeAtStart, timeAtEnd
@@ -28,6 +28,7 @@ program matrix_multi_test
         dim1 = index3
         dim2 = index3
         time(1,index3/step)= index3
+        print *, "Loop ", index3/step, " of ", maxsize/step
 
         ! Allocate x,y,z and fill x and y with values
         allocate(x(dim1,dim2))
@@ -40,89 +41,19 @@ program matrix_multi_test
         ! call write_out_matrixform(y)
 
         ! Matmul_cpu_slow--------------------------------------
-        ! Clock Start:
-        call system_clock(count = count, count_rate = rate)
-        timeAtStart = count / real(rate)
-
-        ! Function:
-        call matmul_cpu_slow(x,y,z)
-
-        ! Clock Stop:
-        call system_clock(count = count, count_rate = rate)
-        timeAtEnd = count / real(rate)
-        
-        ! Save Time:
-        time(2,index3/step)=timeAtEnd-timeAtStart
-        ! call write_out_matrixform(z)
-        !------------------------------------------------------
+        call time_matmul_calc(2,time(2,index3/step))
 
         ! Matmul_cpu_dotmix--------------------------------------
-        ! Clock Start:
-        call system_clock(count = count, count_rate = rate)
-        timeAtStart = count / real(rate)
-
-        ! Function:
-        call matmul_cpu_dotmix(x,y,z)
-
-        ! Clock Stop:
-        call system_clock(count = count, count_rate = rate)
-        timeAtEnd = count / real(rate)
-        
-        ! Save Time:
-        time(3,index3/step)=timeAtEnd-timeAtStart
-        ! call write_out_matrixform(z)
-        !------------------------------------------------------
+        call time_matmul_calc(3,time(3,index3/step))
 
         ! Matmul_cpu_intrinsic--------------------------------------
-        ! Clock Start:
-        call system_clock(count = count, count_rate = rate)
-        timeAtStart = count / real(rate)
+        call time_matmul_calc(4,time(4,index3/step))
 
-        ! Function:
-        call matmul_cpu_intrinsic(x,y,z)
+        ! Matmul_gpu_acc_kernels--------------------------------------
+        call time_matmul_calc(5,time(5,index3/step))
 
-        ! Clock Stop:
-        call system_clock(count = count, count_rate = rate)
-        timeAtEnd = count / real(rate)
-        
-        ! Save Time:
-        time(4,index3/step)=timeAtEnd-timeAtStart
-        ! call write_out_matrixform(z)
-        !------------------------------------------------------
-
-        ! Matmul_cpu_acc_kernels--------------------------------------
-        ! Clock Start:
-        call system_clock(count = count, count_rate = rate)
-        timeAtStart = count / real(rate)
-
-        ! Function:
-        call matmul_cpu_acc_kernels(x,y,z)
-
-        ! Clock Stop:
-        call system_clock(count = count, count_rate = rate)
-        timeAtEnd = count / real(rate)
-        
-        ! Save Time:
-        time(5,index3/step)=timeAtEnd-timeAtStart
-        ! call write_out_matrixform(z)
-        !------------------------------------------------------
-
-        ! Matmul_cpu_acc_loop--------------------------------------
-        ! Clock Start:
-        call system_clock(count = count, count_rate = rate)
-        timeAtStart = count / real(rate)
-
-        ! Function:
-        call matmul_cpu_acc_loop(x,y,z)
-
-        ! Clock Stop:
-        call system_clock(count = count, count_rate = rate)
-        timeAtEnd = count / real(rate)
-        
-        ! Save Time:
-        time(6,index3/step)=timeAtEnd-timeAtStart
-        ! call write_out_matrixform(z)
-        !------------------------------------------------------
+        ! Matmul_gpu_acc_loop--------------------------------------
+        call time_matmul_calc(6,time(6,index3/step))
 
         ! Deallocate x,y,z
         deallocate(x)
@@ -149,5 +80,38 @@ program matrix_multi_test
         end do
     end do
     end subroutine fill_xy_values
+
+    subroutine time_matmul_calc(type, calc_time)
+        integer, intent(in) :: type
+        real(wp), intent(out) :: calc_time
+    
+        ! Clock Start:
+        call system_clock(count = count, count_rate = rate)
+        timeAtStart = count / real(rate)
+
+        ! Function selection:
+        select case (type)
+        case default
+            print *, "No Case Selected! ERROR!"
+        case (2) !matmul slow
+            call matmul_cpu_slow(x,y,z)
+        case (3) !cpu dotmix
+            call matmul_cpu_dotmix(x,y,z)
+        case (4) !cpu intrinsic
+            call matmul_cpu_intrinsic(x,y,z)
+        case (5) !gpu acc kernels
+            call matmul_cpu_acc_kernels(x,y,z)
+        case (6) !gpu acc loop
+            call matmul_cpu_acc_loop(x,y,z)
+        end select
+
+        ! Clock Stop:
+        call system_clock(count = count, count_rate = rate)
+        timeAtEnd = count / real(rate)
+        
+        ! Calculate Time:
+        calc_time = timeAtEnd-timeAtStart
+        
+    end subroutine time_matmul_calc
 
 end program matrix_multi_test
