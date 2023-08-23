f0a08398be1ec71733632d24101c16c38441c5c2
Jaomi Widmann <m300912@levante0.lvt.dkrz.de>
2023-08-23 16:04:40 +0200

    changes in arr_calc_tlab and makefile

--- TlabArrays/array_calc_tlab.f90
+++ TlabArrays/array_calc_tlab.f90
@@ -1,12 +1,11 @@
+! In this program, we want to show
+
 program matrix_multi_test
-    use TLAB_ARRAYS
+    use TLAB_ARRAYS 
     use TLAB_CONSTANTS, only: wp
     implicit none 
-    integer,target :: mat(3,3)
-    ! integer,pointer :: p(:,:)
     integer :: x_dim = 3, y_dim = 3
     
-    mat = reshape([1,2,3,0,4,0,0,0,5],shape(mat))
     allocate(x(x_dim,y_dim))
     allocate(y(x_dim,y_dim))
     x = reshape([1,2,3,0,4,0,0,0,5],[x_dim,y_dim])
@@ -15,10 +14,8 @@ program matrix_multi_test
     ! case 4
     ! does x have to have at least 2 dims in each direction?
 
-    ! x = mat
-    ! y = mat
-    ! z = matmul(x,y)
-    z = mat
+    y = x
+    z = matmul(x,y)
     call write_out_matrixform(z)
 
     ! deallocate(x)
