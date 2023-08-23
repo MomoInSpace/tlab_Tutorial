51ec0b1ba3a0c083806c59390002d18792654419
Jaomi Widmann <m300912@levante0.lvt.dkrz.de>
2023-08-23 16:04:40 +0200

    array_calc pretty

--- TlabArrays/array_calc.f90
+++ TlabArrays/array_calc.f90
@@ -2,48 +2,67 @@ program matrix_multi_test
     use TLAB_ARRAYS
     use TLAB_CONSTANTS, only: wp
     implicit none 
+
+    ! We create a matrix by allocating some space on the disk:
     integer,target :: mat(3,3)
+
+    ! Now we can create a pointer to work on a submatrix later on:
     integer,pointer :: p(:,:)
-    integer :: x_dim = 3, y_dim = 3 
-    ! sf sdf   
+
+    ! Alternatively, we can use allocatables:
+    integer, allocatable :: mat_alloc(:,:)
+    
+    ! Code: ==========================================================
+
+    ! Working with pointers: -------------------------------------------
+    ! Here we set the variables in the matrix mat:
     mat = reshape([1,2,3,0,4,0,0,0,5],shape(mat))
-    allocate(x(x_dim,y_dim))
-    allocate(y(x_dim,y_dim))
-    x = reshape([1,2,3,0,4,0,0,0,5],[x_dim,y_dim])
 
-    
-    ! case 4
-    ! does x have to have at least 2 dims in each direction?
+    ! Because we just want to work on a sumatrix, we set our pointer 
+    ! to said submatrix:
+    p => mat(1:2,1:2)
+    ! Here we output p in matrixform in the terminal:
+    call write_out_matrixform(p)
 
-    ! x = mat
-    ! y = mat
-    ! z = matmul(x,y)
-    z = mat
-    call write_out_matrixform(z)
+    ! Now we want to look at a different part of the matrix:
+    p => mat(2:3,2:3)
+    call write_out_matrixform(p)
 
-    ! deallocate(x)
-    ! deallocate(y)
-    
-    
-     
+    ! Because we don't want to use the pointer anymore, it is safest 
+    ! to let it point to null()
+    p => null()
+
+
+    ! Working with allocatables: -----------------------------------------
+    ! We can use the allocatable similarly to a pointer:
+    mat_alloc = mat(1:2,1:2)
+    call write_out_matrixform(mat_alloc)
+
+    ! Though we could also just allocate a new matrix with the allocatable:
+    deallocate(mat_alloc)
+    allocate(mat_alloc(2,2))
+    ! We would have to fill it with values, then we can print it:
+    mat_alloc = reshape([1,1,1,1],[2,2])
+    call write_out_matrixform(mat_alloc)
+
+    ! The matrix mat is obviously still allocated, just mat_aloc 
+    ! doesn't point there anymore:
+    p => mat(1:2,1:2)
+    call write_out_matrixform(p)
+    p => null()
+
 end program matrix_multi_test
 
 subroutine write_out_matrixform(matrix)
     use TLAB_ARRAYS
     use TLAB_CONSTANTS, only: wp
     implicit none 
-    real(wp), intent(in), dimension (3,3) :: matrix
+    integer, intent(in), dimension (2,2) :: matrix
     integer :: i,j
-    integer, parameter :: n = 3
+    integer, parameter :: n = 2
 
-    ! print *, matrix(1,1) 
-    ! write(*,'(F10.3)')  matrix 
-    ! write(*,'(g0)')  matrix 
+    ! Code: ===============================================================
+
     write( * , "(*(g0.3))" ) ( (matrix(i,j)," ",j=1,n), new_line("A"), i=1,n )
-    ! write(*, *)  matrix 
-    ! print *, matrix(:,2) 
-    ! print *, matrix(:,3) 
-
-
     
 end subroutine write_out_matrixform
