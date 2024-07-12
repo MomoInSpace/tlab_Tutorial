0597eeacef44fbb66fa8d55bf79ea45b81165c01
MomoInSpace <unwissendweilmensch@gmx.de>
2024-07-12 14:58:54 +0200

    test1

--- src/18_mpi_3d_comm/program_comm_test.f90
+++ src/18_mpi_3d_comm/program_comm_test.f90
@@ -1,24 +1,40 @@
-program matrix_multi_test
+program comm_test
     use TLAB_CONSTANTS, only: wp
     use TLAB_ARRAYS 
+    use mpif08
     ! use MATRIX_OUTPUT
     implicit none 
 
-    ! Grid definition
-    INTEGER :: x_cy, y_s, z_cy
-    ! Task Number:
-    INTEGER :: m
+    ! Parameters=============================================================
+    ! Grid definition, sub_grid_y = [x_cy, y_s, z_cy]
+    INTEGER, DIMENSION(3):: sub_grid_y
 
-    INTEGER :: i, num_val, arg_num
-    CHARACTER(len=32) :: arg
+    ! Input Grid Data
+    INTEGER:: i, num_val, arg_num
+    CHARACTER(len = 32):: arg
 
-    DO i = 1, COMMAND_ARGUMENT_COUNT()
-    CALL getarg(i, arg)
+    ! MPI Parameters
+    INTEGER:: my_rank, size
 
-    READ(arg,'(I10)') num_val
-    WRITE(*,*) num_val
-    WRITE(*,*) arg
-    END DO
+    ! Body==================================================================
+    ! Initialisation--------------------------------------------------------
+
+    ! MPI Initialisation
+    CALL MPI_Init()
+    CALL MPI_Comm_rank(MPI_COMM_WORLD, my_rank)
+    CALL MPI_Comm_size(MPI_COMM_WORLD, size)
+
+    IF (my_rank == 0) THEN
+    DO i = 1, COMMAND_ARGUMENT_COUNT()  ! Should be 3
+        CALL getarg(i, arg)
+            READ(arg, '(I10)') sub_grid_y(i)
+        END DO
+    END IF
+    
+    CALL MPI_BCAST(sub_grid_y, 3, MPI_INTEGER, 0, MPI_WORLD)
+
+    WRITE(*,*) sub_grid_y
 
 
-end program matrix_multi_test
+
+end program comm_test
