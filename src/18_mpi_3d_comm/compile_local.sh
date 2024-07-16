9b97c90b0a9b831ce5d044b9e27f772bfd3d9e0f
MomoInSpace <unwissendweilmensch@gmx.de>
2024-07-16 11:05:20 +0200

    renamed compile files

---
+++ src/18_mpi_3d_comm/compile_local.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --account=bb1153
+#STATCH --ntasks=20
+#SBATCH --mem=0
+#SBATCH --time=12:00:00
+#SBATCH --output=Run_10_one_5_%A_%a.out
+#SBATCH --constraint=a100_80
+#SBATCH --exclusive
+
+#--exclusive
+
+# Start of script ========================================
+export gpu_profile="$HOME/Coding/tlab_Tutorial/src/18_mpi_3d_comm"
+cd $gpu_profile
+mkdir build -p 
+cd build
+
+#mpifort $gpu_profile/tlab_constants.f90 $gpu_profile/tlab_arrays.f90 $gpu_profile/export_values.f90 $gpu_profile/timer.f90 $gpu_profile/ring_timer_module.f90 -acc=gpu -target=gpu -Minfo=accel,inline -gpu=lineinfo,cc80 -cpp -o test_ring_times.o
+
+#mpifort $gpu_profile/tlab_constants.f90 $gpu_profile/tlab_arrays.f90 $gpu_profile/export_values.f90 $gpu_profile/timer.f90 $gpu_profile/ring_timer_module.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -o test_ring_times_gpu_only.o
+
+#mpifort $gpu_profile/tlab_constants.f90 $gpu_profile/tlab_arrays.f90 $gpu_profile/export_values.f90 $gpu_profile/timer.f90 $gpu_profile/ring_timer_module.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -o test_ring_times_cpu_only.o
+
+mpif90 $gpu_profile/tlab_constants.f90 $gpu_profile/tlab_arrays.f90 $gpu_profile/grid_utils.f90 $gpu_profile/program_comm_test.f90 -o comm_test.o
+
+
+# Test
+#mpirun -np 5 $gpu_profile/build/test_ring_times.o 3000 250 5 "5only5"
+
+#mpirun -np 10 $gpu_profile/build/test_ring_times.o 3000 250 5 "10only5"
+
+#mpirun -np 15 $gpu_profile/build/test_ring_times.o 3000 250 5 "15only5"
+
+#mpirun -np 20 $gpu_profile/build/test_ring_times.o 3000 250 5 "20only5"
+
+##### SBATCH --mem=24576
+##### SBATCH --nodes=5
+##### SBATCH --ntasks-per-node=2
