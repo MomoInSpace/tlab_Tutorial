c997028019e22fa5bd9d3adbc8e3cd27ed9b87a4
MomoInSpace <unwissendweilmensch@gmx.de>
2025-06-27 11:51:36 +0200

    better sbatch script

--- src/20_grid_comm_gpu/compile_gridhtest.sh
+++ src/20_grid_comm_gpu/compile_gridhtest.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --account=bb1153
-#STATCH --ntasks=20
-#SBATCH --mem=0
+#SBATCH --mem=10g
+#SBATCH --ntasks=16
 #SBATCH --time=12:00:00
-#SBATCH --output=Run_10_one_5_%A_%a.out
+#SBATCH --output=ProfileGPU_Comm.out
 #SBATCH --constraint=a100_80
-#SBATCH --exclusive
 
 #--exclusive
 
@@ -16,25 +15,19 @@ cd $gpu_profile
 mkdir build -p
 cd build
 
-#mpifort $gpu_profile/tlab_constants.f90 $gpu_profile/tlab_arrays.f90 $gpu_profile/export_values.f90 $gpu_profile/timer.f90 $gpu_profile/ring_timer_module.f90 -acc=gpu -target=gpu -Minfo=accel,inline -gpu=lineinfo,cc80 -cpp -o test_ring_times.o
-
-#mpifort $gpu_profile/tlab_constants.f90 $gpu_profile/tlab_arrays.f90 $gpu_profile/export_values.f90 $gpu_profile/timer.f90 $gpu_profile/ring_timer_module.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -o test_ring_times_gpu_only.o
-
-#mpifort $gpu_profile/tlab_constants.f90 $gpu_profile/tlab_arrays.f90 $gpu_profile/export_values.f90 $gpu_profile/timer.f90 $gpu_profile/ring_timer_module.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -o test_ring_times_cpu_only.o
-
-mpifort $gpu_profile/tlab_constants.cuf $gpu_profile/tlab_arrays.cuf $gpu_profile/grid_handler.cuf $gpu_profile/grid_communicator.cuf $gpu_profile/grid_debug.cuf  $gpu_profile/program_gridhtest.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -g -O0 -Minfo=accel,inline -o comm_test.o
-
-
+mpifort $gpu_profile/tlab_constants.cuf $gpu_profile/tlab_arrays.cuf $gpu_profile/grid_handler.cuf $gpu_profile/grid_communicator.cuf $gpu_profile/grid_debug.cuf  $gpu_profile/program_gridhtest.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -g -O0 -Minfo=accel,inline -o comm_test.run
 
 # Test
-#mpirun -np 5 $gpu_profile/build/test_ring_times.o 3000 250 5 "5only5"
-
-#mpirun -np 10 $gpu_profile/build/test_ring_times.o 3000 250 5 "10only5"
-
-#mpirun -np 15 $gpu_profile/build/test_ring_times.o 3000 250 5 "15only5"
-
-#mpirun -np 20 $gpu_profile/build/test_ring_times.o 3000 250 5 "20only5"
+#mpirun -np 4 $gpu_profile/build/comm_test.o 10 4 10
+#
+#mpirun -np 8 $gpu_profile/build/comm_test.o 10 4 10
+#
+#mpirun -np 12 $gpu_profile/build/comm_test.o 10 4 10
+#
+#mpirun -np 16 $gpu_profile/build/comm_test.o 10 4 10
 
 ##### SBATCH --mem=24576
-##### SBATCH --nodes=5
 ##### SBATCH --ntasks-per-node=2
+##### SBATCH --exclusive
+##### SBATCH --nodes=4
+##### SBATCH --ntasks-per-node=4
