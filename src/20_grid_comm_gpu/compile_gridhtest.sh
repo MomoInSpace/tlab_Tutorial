5accd9e4676d2cf9098236667c976c8e70c5c07c
MomoInSpace <unwissendweilmensch@gmx.de>
2025-09-17 16:26:39 +0200

    Output for same size run and big grid run

--- src/20_grid_comm_gpu/compile_gridhtest.sh
+++ src/20_grid_comm_gpu/compile_gridhtest.sh
@@ -45,29 +45,29 @@ mpifort  tlab_constants.o  tlab_arrays.o  grid_handler.o  $gpu_profile/grid_hand
 mpifort  tlab_constants.o  tlab_arrays.o  grid_handler.o  $gpu_profile/grid_handler_gpu.cuf  grid_handler_cpu.o  grid_communicator.o  $gpu_profile/grid_communicator_gpu.cuf  grid_communicator_cpu.o  grid_debug.o   grid_ops.o  $gpu_profile/program_test_diff_cpu.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -g -O0 -Minfo=accel,inline -o diff_test_cpu.run
 
 # Snabla GPU
-mpifort  tlab_constants.o  tlab_arrays.o  grid_handler.o  $gpu_profile/grid_handler_gpu.cuf  grid_handler_cpu.o  grid_communicator.o  $gpu_profile/grid_communicator_gpu.cuf  grid_communicator_cpu.o  grid_debug.o   grid_ops.o  $gpu_profile/program_simple_nabla_gpu.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -g -O0 -Minfo=accel,inline -o diff_test_gpu.run
+mpifort  tlab_constants.o  tlab_arrays.o  grid_handler.o  $gpu_profile/grid_handler_gpu.cuf  grid_handler_cpu.o  grid_communicator.o  $gpu_profile/grid_communicator_gpu.cuf  grid_communicator_cpu.o  grid_debug.o   grid_ops.o  $gpu_profile/program_test_diff_gpu.cuf -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -g -O0 -Minfo=accel,inline -o diff_test_gpu.run
 #mpifort $gpu_profile/tlab_constants.cuf $gpu_profile/tlab_arrays.cuf $gpu_profile/grid_handler.cuf $gpu_profile/grid_handler_gpu.cuf $gpu_profile/grid_handler_cpu.cuf $gpu_profile/grid_communicator.cuf $gpu_profile/grid_communicator_gpu.cuf $gpu_profile/grid_communicator_cpu.cuf $gpu_profile/grid_debug.cuf  $gpu_profile/grid_ops.cuf $gpu_profile/program_simple_nabla_gpu.cuf  -acc=gpu -target=gpu -gpu=lineinfo,cc80 -cpp -g -O0 -Minfo=accel,inline -o comm_snabla_gpu.run
 
 # Test
 #echo "N4 CPU Run"
-#mpirun -n 4 comm_snabla_cpu.run 16 4 16
+#mpirun -n 4 diff_test_cpu.run 16 8 32
 #echo "N4 GPU Run"
-#mpirun -n 4 comm_snabla_gpu.run 16 4 16
+#mpirun -n 4 diff_test_gpu.run 16 8 32
 #
 #echo "N8 CPU Run"
-#mpirun -n 8 comm_snabla_cpu.run 16 4 16
+#mpirun -n 8 diff_test_cpu.run 8 8 32
 #echo "N8 GPU Run"
-#mpirun -n 8 comm_snabla_gpu.run 16 4 16
-#
+#mpirun -n 8 diff_test_gpu.run 8 8 32
+
 #echo "N12 CPU Run"
-#mpirun -n 12 comm_snabla_cpu.run 16 4 16
+#mpirun -n 12 diff_test_cpu.run 16 4 16
 #echo "N12 GPU Run"
-#mpirun -n 12 comm_snabla_gpu.run 16 4 16
-#
+#mpirun -n 12 diff_test_gpu.run 16 4 16
+
 #echo "N16 CPU Run"
-#mpirun -n 16 comm_snabla_cpu.run 16 4 16
+#mpirun -n 16 diff_test_cpu.run 4 2 8
 #echo "N16 GPU Run"
-#mpirun -n 16 comm_snabla_gpu.run 16 4 16
+#mpirun -n 16 diff_test_gpu.run 4 2 8
 
 ##### SBATCH --mem=24576
 ##### SBATCH --ntasks-per-node=2
---
+++ src/20_grid_comm_gpu/runs/diff_test_big.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --account=bb1153
+#SBATCH --ntasks=16
+#SBATCH --gpus-per-task=1
+#SBATCH --time=1:00:00
+#SBATCH --output=diff_test_huge.out
+#SBATCH --constraint=a100_80
+#SBATCH --mem=5g
+#SBATCH --exclusive
+
+#--exclusive
+
+# Start of script ========================================
+export gpu_profile="$HOME/fortran_projs/tlab_Tutorial/src/20_grid_comm_gpu"
+#cd $gpu_profile
+#mkdir build -p
+#cd build
+
+# Test
+#echo "N4 CPU Run"
+#mpirun -n 4 $gpu_profile/build/diff_test_cpu.run 16 8 32
+#echo "N4 GPU Run"
+#mpirun -n 4 $gpu_profile/build/diff_test_gpu.run 16 8 32
+#
+#echo "N8 CPU Run"
+#mpirun -n 8 $gpu_profile/build/diff_test_cpu.run 8 8 32
+#echo "N8 GPU Run"
+#mpirun -n 8 $gpu_profile/build/diff_test_gpu.run 8 8 32
+
+#echo "N12 CPU Run"
+#mpirun -n 12 diff_test_cpu.run 16 4 16
+#echo "N12 GPU Run"
+#mpirun -n 12 diff_test_gpu.run 16 4 16
+
+echo "N16 CPU Run"
+time mpirun -n 16 $gpu_profile/build/diff_test_cpu.run 4 2 8
+echo "N16 GPU Run"
+time mpirun -n 16 $gpu_profile/build/diff_test_cpu.run 4 2 8
+
+echo "N16 CPU Run"
+time mpirun -n 16 $gpu_profile/build/diff_test_cpu.run 8 4 16
+echo "N16 GPU Run"
+time mpirun -n 16 $gpu_profile/build/diff_test_gpu.run 8 4 16
+
+echo "N16 CPU Run"
+time mpirun -n 16 $gpu_profile/build/diff_test_cpu.run 16 8 32
+echo "N16 GPU Run"
+time mpirun -n 16 $gpu_profile/build/diff_test_gpu.run 16 8 32
+
+echo "N16 CPU Run"
+time mpirun -n 16 $gpu_profile/build/diff_test_cpu.run 32 16 64
+echo "N16 GPU Run"
+time mpirun -n 16 $gpu_profile/build/diff_test_gpu.run 32 16 64
+
+##### SBATCH --mem=24576
+##### SBATCH --ntasks-per-node=2
+##### SBATCH --exclusive
+##### SBATCH --ntasks-per-node=4
