7099af33d33db724d3475f86f2c51c42e16f3786
MomoInSpace <unwissendweilmensch@gmx.de>
2025-09-19 13:35:27 +0200

    Better py output

--- src/20_grid_comm_gpu/runs/diff_test_same_size2.sh
+++ src/20_grid_comm_gpu/runs/diff_test_same_size2.sh
@@ -18,14 +18,14 @@ export gpu_profile="$HOME/fortran_projs/tlab_Tutorial/src/20_grid_comm_gpu"
 #cd build
 
 # Test
-echo "N4 CPU Run"
+echo "# N4 CPU Run"
 mpirun -n 4 $gpu_profile/build/diff_test_cpu.run 16 8 32
-echo "N4 GPU Run"
+echo "# N4 GPU Run"
 mpirun -n 4 $gpu_profile/build/diff_test_gpu.run 16 8 32
 
-echo "N8 CPU Run"
+echo "# N8 CPU Run"
 mpirun -n 8 $gpu_profile/build/diff_test_cpu.run 8 8 32
-echo "N8 GPU Run"
+echo "# N8 GPU Run"
 mpirun -n 8 $gpu_profile/build/diff_test_gpu.run 8 8 32
 
 #echo "N12 CPU Run"
@@ -33,9 +33,9 @@ mpirun -n 8 $gpu_profile/build/diff_test_gpu.run 8 8 32
 #echo "N12 GPU Run"
 #mpirun -n 12 diff_test_gpu.run 16 4 16
 
-echo "N16 CPU Run"
+echo "# N16 CPU Run"
 mpirun -n 16 $gpu_profile/build/diff_test_cpu.run 4 2 8
-echo "N16 GPU Run"
+echo "# N16 GPU Run"
 mpirun -n 16 $gpu_profile/build/diff_test_gpu.run 4 2 8
 
 
